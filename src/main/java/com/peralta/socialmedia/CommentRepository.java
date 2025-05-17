3d134c861572eb2a2a9118fd60bc1074abbd88b6
ChristianDPeralta <christianperalta656@gmail.com>
2025-05-17 16:49:28 +0800

    Update CommentRepository.java

--- src/main/java/com/peralta/socialmedia/CommentRepository.java
+++ src/main/java/com/peralta/socialmedia/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.peralta.socialmedia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
 }
